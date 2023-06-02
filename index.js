39aada9f898cede049608c992f2c9d3b6eb291a1
鸿能 <leiqi.lq@alibaba-inc.com>
2023-06-02 21:05:01 +0800

    open ai

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const {
   AI_TYPE_IMAGE,
 } = require('./db');
 
-const { sleep, strip } = require('./utils');
-
 const router = new Router();
 
 const homePage = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
@@ -35,6 +33,20 @@ const LIMIT_AI_TEXT_COUNT = 10;
 const LIMIT_AI_IMAGE_COUNT = 5;
 
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
+
+const sleep = time =>
+  new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+
+// 去头尾指定字符
+const strip = (str, chars) => {
+  let newStr = str;
+  chars.forEach(char => {
+    newStr = newStr.replace(new RegExp(`^${char}+|${char}+$`, 'g'), '');
+  });
+  return newStr;
+};
 
 const configuration = new Configuration({
   apiKey: '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',
@@ -64,7 +76,7 @@ async function getAIResponse(prompt) {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt,
-    max_tokens: 1024,
+    max_tokens: 30,
     temperature: 0.1,
   });
 
